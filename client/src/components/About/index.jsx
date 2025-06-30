8b0b04d4ee65dfefe3f275223303b3a6ee3dea68
gourabop3 <gourabmullick200@gmail.com>
2025-06-30 20:57:47 +0530

    Update index.jsx

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -11,7 +11,7 @@ const About = () => {
         return (
           <>
             <div className="about-page" key={index}>
-              <AboutHero image={AboutIMG} id={item.id} />
+              <AboutHero image={#} id={item.id} />
               <AboutContent
                 id={item.id}
                 title={item.title}
